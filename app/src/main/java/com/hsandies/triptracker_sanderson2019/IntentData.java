1865d1103c38ec152ff1d9f63269c1d6ef47b5b4
SpicyF1sh <haydensand011@gmail.com>
2019-05-23 10:18:44 -0400

    Initial commit

---
+++ app/src/main/java/com/hsandies/triptracker_sanderson2019/IntentData.java
@@ -0,0 +1,11 @@
+package com.hsandies.triptracker_sanderson2019;
+
+public interface IntentData {
+    String EXTRA_TRIP_ID = "com.hsandies.triptracker_sanderson2019.TRIP_ID";
+    String EXTRA_TRIP_NAME = "com.hsandies.triptracker_sanderson2019.TRIP_NAME";
+    String EXTRA_TRIP_DESC = "com.hsandies.triptracker_sanderson2019.TRIP_DESC";
+    String EXTRA_TRIP_START_DATE = "com.hsandies.triptracker_sanderson2019.TRIP_START_DATE";
+    String EXTRA_TRIP_END_DATE = "com.hsandies.triptracker_sanderson2019.TRIP_END_DATE";
+    String EXTRA_TRIP_PUBLIC = "com.hsandies.triptracker_sanderson2019.TRIP_PUBLIC";
+    String EXTRA_TRIP_PUBLIC_VIEW = "com.hsandies.triptracker_sanderson2019.TRIP_PUBLIC_VIEW";
+}
---
+++ app/src/main/java/com/hsandies/triptracker_sanderson2019/Trip.java
@@ -0,0 +1,64 @@
+package com.hsandies.triptracker_sanderson2019;
+
+
+import java.util.Date;
+
+public class Trip implements IntentData{
+    String mObjectId;
+    static String mName;
+    String mDescription;
+    static Date mStartDate;
+    Date mEndDate;
+
+    public Boolean isShared() {
+        return mShared;
+    }
+
+    public void setShared(Boolean shared) {
+        mShared = shared;
+    }
+
+    Boolean mShared;
+
+    public String getObjectId() {
+        return mObjectId;
+    }
+
+    public void setObjectId(String objectId) {
+        mObjectId = objectId;
+    }
+
+    public static String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public static Date getStartDate() {
+        return mStartDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        mStartDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return mEndDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        mEndDate = endDate;
+    }
+
+
+}
